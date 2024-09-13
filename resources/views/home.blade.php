e9c8afab37003c4b61fe6aa215e561279c612d91
ODUKOI EMMANUEL JOSHUA <jashemma6@gmail.com>
2024-09-13 16:46:47 +0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets\css\style.css">
+</head>
+<body>
+    <nav>
+        <a href="{{route('home')}}">HOME</a>
+        <a href="{{route('about')}}">ABOUT</a>
+    </nav>
+    <h2>welcome</h2>
+</body>
+</html>
